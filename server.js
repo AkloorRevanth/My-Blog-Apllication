452719ab4b8bb21c11241e3748345ca9437405f8
Akloor Revanth <arevanth27@gmail.com>
2025-07-19 14:52:07 +0530

    Fix: Removed duplicate 'path' import

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const multer = require('multer');
-const path = require('path');
 const Post = require('./models/Post');
 
 const app = express();
